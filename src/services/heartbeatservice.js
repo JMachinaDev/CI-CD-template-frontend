1944cf0b597056aa657c809f400ca3e30f47d19c
jmachinadev <jmachinadev@gmail.com>
2021-06-04 14:07:17 -0400

    add another object for '/test' route

--- src/services/heartbeatservice.js
+++ src/services/heartbeatservice.js
@@ -1,11 +1,15 @@
 import http from '../http-common';
 
+// retrieve data/routes from server/api
 class HeartbeatService {
   ping() {
     console.log('Successfully checked API heartbeat, connection established')
     return http.get('/');
   };
-
+  pingTest() {
+    console.log('Successfully checked test route');
+    return http.get('/test');
+  }
 };
 
 export default HeartbeatService;
